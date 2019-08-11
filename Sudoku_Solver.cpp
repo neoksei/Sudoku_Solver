95caf5c4b7e27fc48533a5facebc06063727e5ff
Aleksei Ksei <40474927+ssssalem@users.noreply.github.com>
2019-08-11 19:28:10 +0300

    Update Sudoku_Solver.cpp

--- Sudoku_Solver.cpp
+++ Sudoku_Solver.cpp
@@ -20,48 +20,47 @@ void print_matrix(int** matrix) {
 	std::cout << "\n_________________\n";
 }
 
-int_pair find_free_cell(int** matrix) { // ищет свободную клетку
+int_pair find_free_cell(int** matrix) { // return a free cell
 	int_pair cell;
 	cell.i = 666;
 	cell.j = 666;
 	for (int i = 0; i < 9; i++)
 		for (int j = 0; j < 9; j++) {
-			if (matrix[i][j] == 0) { // свободной считается зануленная клетка
+			if (matrix[i][j] == 0) { // find a free cell
 				cell.i = i;
 				cell.j = j;
 				return cell;
 			}
 		}
-	return cell; // если свободных клеток не осталось, возвращается клетка со специальным значением
-}
+	return cell; // if there isn't a free cell, returns special value
 
-void find_fit_digits(int** matrix, int_pair cell, int* digits) { // по клетке ищет возможные цифры
-	// возвращает возможные цифры в массиве digits
+void find_fit_digits(int** matrix, int_pair cell, int* digits) { // resulting digits will be returned in array
+	
 	for (int i = 0; i < 9; i++) {
-		digits[i] = i + 1; // изначально считаем, что любая цифра подходит
-	}					   // цифры смещены на единицу относительно индекса
-
-	for (int j = 0; j < 9; j++) { // сначала смотрим на строку
-		if (matrix[cell.i][j] != 0) { // 
-			digits[matrix[cell.i][j] - 1] = 0; // не забываем про смещение
+		digits[i] = i + 1; // all digits fit first
+	}
+	
+	for (int j = 0; j < 9; j++) { // chech rows
+		if (matrix[cell.i][j] != 0) { // if there is some digit
+			digits[matrix[cell.i][j] - 1] = 0; // it doesnt fit
 		}
 	}
 
-	for (int i = 0; i < 9; i++) { // теперь на столбец
+	for (int i = 0; i < 9; i++) { // check column
 		if (matrix[i][cell.j] != 0) {
 			digits[matrix[i][cell.j] - 1] = 0;
 		}
 	}
 
-	for (int i = cell.i / 3 * 3; i < cell.i / 3 * 3 + 3; i++) // теперь на квадрант
+	for (int i = cell.i / 3 * 3; i < cell.i / 3 * 3 + 3; i++) // check square
 		for (int j = cell.j / 3 * 3; j < cell.j / 3 * 3 + 3; j++) {
 			if (matrix[i][j] != 0) {
 				digits[matrix[i][j] - 1] = 0;
 			}
-		} // теперь в массиве только подходящие цифры и нули
+		} // Now there are only suitable digits in our array
 }
 
-int** create_matrix() { // стандартный такой create_matrix
+int** create_matrix() { 
 	int** A;
 	if (!(A = (int**)malloc(sizeof(int*) * 9))) {
 		std::cout << "Error allocation memory (Row)" << std::endl;
@@ -76,7 +75,7 @@ int** create_matrix() { // стандартный такой create_matrix
 
 	for (int i = 0; i < 9; i++)
 		for (int j = 0; j < 9; j++) {
-			A[i][j] = 0; // изначально любая клетка свободна
+			A[i][j] = 0; // All cells a free by default
 		}
 
 	return A;
@@ -84,32 +83,32 @@ int** create_matrix() { // стандартный такой create_matrix
 
 void solve(int** matrix) {
 	static bool solution_was_found = false;
-	int_pair cell = find_free_cell(matrix); // свободная клетка
+	int_pair cell = find_free_cell(matrix); 
 
-	if (cell.i == 666) { // свободных клеток не осталось
-		solution_was_found = true; // решение было найдено
+	if (cell.i == 666) { // it means there isn't a free cell
+		solution_was_found = true; // So solution was found
 		return;
 	}
 
 	int digits[9];
 
-	find_fit_digits(matrix, cell, digits); // нашли подходящие цифры
+	find_fit_digits(matrix, cell, digits);
 
-	for (int i = 0; i < 9; i++) { // перебираем их в цикле
-		if (solution_was_found) { // если решение было найдено ранее
-			return; // сворачиваем функцию
+	for (int i = 0; i < 9; i++) {
+		if (solution_was_found) { // Must check it for each iteration
+			return; // It will collapse all functions
 		}
-		if (digits[i] != 0) { // если есть подходящая цифра
-			matrix[cell.i][cell.j] = digits[i]; //подставляем ее в матрицу
-			solve(matrix); // шаг рекурсии
-		} // если цифр не оказалось (решение зашло в тупик), матрица остается без изменений
-	}     // а функция просто покидается
+		if (digits[i] != 0) {
+			matrix[cell.i][cell.j] = digits[i]; // It's our prediction
+			solve(matrix); // step of a recursion
+		}
+	}
 	
-	if (solution_was_found) { //если на последней итерации цифра оказалось подходящей
-		return; // важно выйти без зануления
+	if (solution_was_found) { // If we leave cycle when solution was found
+		return; // We collapse our function
 	}
 	else {
-		matrix[cell.i][cell.j] = 0; // иначе в финальной матрице будут встречаться рандомные нули
+		matrix[cell.i][cell.j] = 0; // If solution was not found, it's important to leave matrix unchanged
 	}
 
 }
